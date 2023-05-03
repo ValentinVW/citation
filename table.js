3000424706d0c15fc50c9adf409a8e8dbebc7e31
Valentin VW <valentinvanweydevelt@hotmail.com>
2023-05-03 21:44:13 +0200

    first-commit

---
+++ table.js
@@ -0,0 +1,11 @@
+let citation = [
+  "Ta Maman t'embrasse.",
+  "Certaines erreurs méritent d'être commises plusieurs fois.",
+  "Un jour tu es le pigeon, le lendemain la statue. Aujourd'hui, t'es plutôt côté statue.",
+  "J'aime l'odeur du gâteau au petit matin. ça sent comme... Le gâteau.",
+  "Bonne nouvelle, vous n'êtes pas illetré.",
+  "Vous êtes le meilleur ne vous laissez pas abattre. Restez à couvert.",
+  "Vis chaque jour comme si c'était le dernier. Ou du moins, aujourd'hui parce que ... Non Désolé, je veux pas gâcher la surprise.",
+  "On est jamis trop vieux pour apprendre des conneries",
+  "",
+];
