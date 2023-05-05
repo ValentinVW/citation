7add0e5af9d186bf249688d5043c53a343935ac9
Valentin VW <valentinvanweydevelt@hotmail.com>
2023-05-05 18:09:57 +0200

    add-packagejson-and-webpack-and-src

--- src/table.js
+++ src/table.js
@@ -6,6 +6,9 @@ let citation = [
   "Bonne nouvelle, vous n'êtes pas illetré.",
   "Vous êtes le meilleur ne vous laissez pas abattre. Restez à couvert.",
   "Vis chaque jour comme si c'était le dernier. Ou du moins, aujourd'hui parce que ... Non Désolé, je veux pas gâcher la surprise.",
-  "On est jamis trop vieux pour apprendre des conneries",
-  "",
+  "On est jamais trop vieux pour apprendre des conneries",
+  "La lumière voyage plus vite que le son. C'est pourquoi certaines personnes ont l'air brillantes, jusqu'à ce qu'on les entende parler.",
+  "Tout n'est pas politique, mais la politique s'intéresse à tout.",
+  "Vous cesserez de procrastiner. Plus tard.",
+  "Rien de mieux que le chaos pour se sentir vivant.",
 ];
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: path.join(__dirname, "src/index.js"),
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name].bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        exclude: /(node_modules)/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.scss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "./src/assets/images/*",
+          to: "assets/images/[name][ext]",
+        },
+      ],
+    }),
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: path.join(__dirname, "./src/index.html"),
+      chunks: ["main"],
+    }),
+  ],
+  stats: "minimal",
+  devtool: "source-map",
+  mode: "development",
+  devServer: {
+    static: path.resolve(__dirname, "./dist"),
+    open: true,
+    port: 4000,
+    watchFiles: ["./src/**"],
+    hot: true,
+  },
+};
